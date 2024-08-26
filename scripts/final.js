fa97e1b4b3f83c796680326ab72f38a062e85010
priscila-yone-fujii <90528166+priscila-fujii@users.noreply.github.com>
2024-08-26 13:51:59 -0300

    Atualizar o final.js

--- scripts/final.js
+++ scripts/final.js
@@ -1,8 +1,6 @@
 const btn12 = document.querySelector('#p12');
-const corpo = document.body;
 
 
 btn12.addEventListener("click", () =>{
-    corpo.style.background = "../qualquer/img/resolvido.png";
-    corpo.style.color = "#ffa600"
+    window.location.replace('resolvido.html');
 });
